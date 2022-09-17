b29019d67abf0ec17fc3e08da595c9cfe61ef387
Patrik Zehle <106169411+Patrik-Zehle@users.noreply.github.com>
2022-09-17 16:06:15 +0200

    In progress 16.9. 22

---
+++ main.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-butotn-prev",
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+});
